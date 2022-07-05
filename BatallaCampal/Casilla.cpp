549a563135abce06c743a27a94d8d8dedbd52f26
Martina Souza <martina.f.souza@gmail.com>
2022-07-05 09:58:12 -0300

    arreglo error de sintaxis y formato

--- BatallaCampal/Casilla.cpp
+++ BatallaCampal/Casilla.cpp
@@ -1,75 +1,74 @@
 #include "Casilla.h"
 #include "Ficha.h"
 
-Casilla::Casilla(unsigned int coordenadaX, unsigned int coordenadaY, unsigned int coordenadaZ, TipoDeCasillero tipo){
+Casilla::Casilla(unsigned int coordenadaX, unsigned int coordenadaY,
+		unsigned int coordenadaZ, TipoDeCasillero tipo) {
 
-	setCoordenada(this->coordenadaX,coordenadaX);
-	setCoordenada(this->coordenadaY,coordenadaY);
-	setCoordenada(this->coordenadaZ,coordenadaZ);
+	setCoordenada(this->coordenadaX, coordenadaX);
+	setCoordenada(this->coordenadaY, coordenadaY);
+	setCoordenada(this->coordenadaZ, coordenadaZ);
 
 	setTipoDeCasillero(tipo);
 	setEstado(VACIO);
 }
 
-void Casilla::setCoordenada(unsigned int &coordenadaAModificar, unsigned int coordenada){
-		if(coordenada > 0){
-			coordenadaAModificar = coordenada;
-		}
-		else{
-			throw "Las coordenadas de un casillero no pueden ser negativas.";
-		}
+void Casilla::setCoordenada(unsigned int &coordenadaAModificar,
+		unsigned int coordenada) {
+	if (coordenada > 0) {
+		coordenadaAModificar = coordenada;
+	} else {
+		throw "Las coordenadas de un casillero no pueden ser negativas.";
 	}
+}
 
-void Casilla::setTipoDeCasillero(TipoDeCasillero tipo){
-	if(tipo == TIERRA || tipo == AGUA || tipo == AIRE || tipo == GENERAL){
+void Casilla::setTipoDeCasillero(TipoDeCasillero tipo) {
+	if (tipo == TIERRA || tipo == AGUA || tipo == AIRE || tipo == GENERAL) {
 		this->tipoDeCasilla = tipo;
-	}
-	else{
+	} else {
 		throw "El tipo de casilla ingresado es inválida. Puede ser: AVION, BARCO, SOLDADO o de tipo uso GENERAL.";
 	}
-	
+
 }
 
-void Casilla::setEstado(EstadoDelCasillero estadoCasilla){
-    if(estadoCasilla == VACIO || estadoCasilla == LLENO || estadoCasilla == INACTIVO){
-        this->estadoCasilla = estadoCasilla;
-        if(estadoCasilla == VACIO){
-        	this->estadoActual = VACIA;
-        }else if(estadoCasilla == LLENO){
-        	this->estadoActual = LLENA;
-        }else if(estadoCasilla == INACTIVO){
-        	this->estadoActual = INACTIVA;
-        }else if(estadoCasilla == MINADO){
-		this->estadoActual == MINADO;
+void Casilla::setEstado(EstadoDelCasillero estadoCasilla) {
+	if (estadoCasilla == VACIO || estadoCasilla == LLENO
+			|| estadoCasilla == INACTIVO) {
+		this->estadoCasilla = estadoCasilla;
+		if (estadoCasilla == VACIO) {
+			this->estadoActual = VACIA;
+		} else if (estadoCasilla == LLENO) {
+			this->estadoActual = LLENA;
+		} else if (estadoCasilla == INACTIVO) {
+			this->estadoActual = INACTIVA;
+		} else if (estadoCasilla == MINADO) {
+			this->estadoActual = MINADO;
+		}
+	} else {
+		throw "El contenido es invalido, debe ser: VACIO, FULL, INACTIVO o MINADO";
 	}
-    }
-    else{
-        throw "El contenido es invalido, debe ser: VACIO, FULL, INACTIVO o MINADO";
-    }
 }
 
-EstadoDelCasillero Casilla::getEstado(){
+EstadoDelCasillero Casilla::getEstado() {
 	return this->estadoCasilla;
 }
 
-TipoDeCasillero Casilla::getTipoDeCasilla(){
-    return this->tipoDeCasilla;
+TipoDeCasillero Casilla::getTipoDeCasilla() {
+	return this->tipoDeCasilla;
 }
 
-unsigned int Casilla::getCoordenadaX(){
-    return this->coordenadaX;
+unsigned int Casilla::getCoordenadaX() {
+	return this->coordenadaX;
 }
 
-unsigned int Casilla::getCoordenadaY(){
-    return this->coordenadaY;
+unsigned int Casilla::getCoordenadaY() {
+	return this->coordenadaY;
 }
 
-
-unsigned int Casilla::getCoordenadaZ(){
-    return this->coordenadaZ;
+unsigned int Casilla::getCoordenadaZ() {
+	return this->coordenadaZ;
 }
 
-std::string Casilla::getEstadoActual(){
+std::string Casilla::getEstadoActual() {
 	return this->estadoActual;
 }
 
--- BatallaCampal/Jugador.cpp
+++ BatallaCampal/Jugador.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-Jugador::Jugador(unsigned int id, int cantidadSoldados){
-	if(id < 1){
+Jugador::Jugador(unsigned int id, int cantidadSoldados) {
+	if (id < 1) {
 		throw "El id de jugador debe ser un número mayor o igual que uno.";
 	}
-	if(cantidadSoldados < 1){
+	if (cantidadSoldados < 1) {
 		throw "Cantidad inválida de soldados para el jugador.";
 	}
-	
+
 	this->id = id;
 	this->cantidadSoldados = cantidadSoldados;
 	this->cantidadHerramientas = 0;
@@ -19,40 +19,39 @@ Jugador::Jugador(unsigned int id, int cantidadSoldados){
 	this->estado = EN_BATALLA;
 }
 
-unsigned int Jugador::getCantidadDeHerramientas(){
+unsigned int Jugador::getCantidadDeHerramientas() {
 	return this->herramientas->contarElementos();
 }
 
-int Jugador::getCantidadDeSoldados(){
+int Jugador::getCantidadDeSoldados() {
 	return this->soldados->contarElementos();
-
 }
 
-unsigned int Jugador::getCantidadDeCartas(){
+unsigned int Jugador::getCantidadDeCartas() {
 	return this->cartas->contarElementos();
 }
 
-Lista<Ficha*>* Jugador::getHerramienta(){
+Lista<Ficha*>* Jugador::getHerramienta() {
 	return this->herramientas;
 }
 
-Lista<Ficha*>* Jugador::getSoldado(){
+Lista<Ficha*>* Jugador::getSoldado() {
 	return this->soldados;
 }
 
-Lista<Carta*>* Jugador::getCarta(){
+Lista<Carta*>* Jugador::getCarta() {
 	return this->cartas;
 }
 
-unsigned int Jugador::buscarIdSoldado(int fila, int columna){
-	if(fila < 1 || columna < 1){
+unsigned int Jugador::buscarIdSoldado(int fila, int columna) {
+	if (fila < 1 || columna < 1) {
 		throw "No se puede buscar un soldado en una fila o columna que no tenga valor al menos 1.";
 	}
-	
+
 	bool encontrado = false;
 	this->soldados->reiniciarCursor();
 	int id = 0;
-	while (this->soldados->avanzarCursor() && (!encontrado)){
+	while (this->soldados->avanzarCursor() && (!encontrado)) {
 		id++;
 		if (this->soldados->getCursor()->getPosicionX() == fila
 				&& this->soldados->getCursor()->getPosicionY() == columna) {
@@ -63,24 +62,24 @@ unsigned int Jugador::buscarIdSoldado(int fila, int columna){
 	return id;
 }
 
-int Jugador::buscarIdHerramienta(int fila, int columna){
-	if(fila < 1 || columna < 1){
+int Jugador::buscarIdHerramienta(int fila, int columna) {
+	if (fila < 1 || columna < 1) {
 		throw "No se puede buscar un soldado en una fila o columna que no tenga valor al menos 1.";
 	}
 	bool herramientaEncontrada = false;
 	int idHerramienta = 0;
-	while(this->herramientas->avanzarCursor() && (!herramientaEncontrada)){
+	while (this->herramientas->avanzarCursor() && (!herramientaEncontrada)) {
 		idHerramienta++;
-		if(this->herramientas->getCursor()->getPosicionX() == fila
-		 	&& this->herramientas->getCursor()->getPosicionY() == columna){
-		 		
-				herramientaEncontrada = true;
-			}
+		if (this->herramientas->getCursor()->getPosicionX() == fila
+				&& this->herramientas->getCursor()->getPosicionY() == columna) {
+
+			herramientaEncontrada = true;
+		}
 	}
 	return idHerramienta;
 }
 
-bool Jugador::buscarSoldado(int fila, int columna){
+bool Jugador::buscarSoldado(int fila, int columna) {
 	bool encontrado = false;
 	this->soldados->reiniciarCursor();
 	while (this->soldados->avanzarCursor() && (!encontrado)) {
@@ -93,7 +92,7 @@ bool Jugador::buscarSoldado(int fila, int columna){
 	return encontrado;
 }
 
-bool Jugador::buscarHerramienta(int fila, int columna){
+bool Jugador::buscarHerramienta(int fila, int columna) {
 	bool HerramientaEncontrada = false;
 	this->herramientas->reiniciarCursor();
 	while (this->herramientas->avanzarCursor() && (!HerramientaEncontrada)) {
@@ -104,92 +103,92 @@ bool Jugador::buscarHerramienta(int fila, int columna){
 		}
 	}
 	return HerramientaEncontrada;
-	
+
 }
 
-void Jugador::setEstado(EstadoJugador estado){
-	if(estado != EN_BATALLA && estado != ELIMINADO){
+void Jugador::setEstado(EstadoJugador estado) {
+	if (estado != EN_BATALLA && estado != ELIMINADO) {
 		throw "El estado del jugador ingresado es inválido";
 	}
 	this->estado = estado;
 }
 
-EstadoJugador Jugador::getEstadoJugador(){
+EstadoJugador Jugador::getEstadoJugador() {
 	return this->estado;
 }
 
-void Jugador::nuevaCarta(int aleatorio){
-	if(aleatorio < 0){
+void Jugador::nuevaCarta(int aleatorio) {
+	if (aleatorio < 0) {
 		throw "El número de carta no puede ser negativo";
 	}
-	
-	if (this->cartas->contarElementos() < MAX_CARTAS){
+
+	if (this->cartas->contarElementos() < MAX_CARTAS) {
 		this->cartas->add(new Carta(aleatorio));
 	}
 }
 
-unsigned int Jugador::getId(){
+unsigned int Jugador::getId() {
 	return this->id;
 }
 
-void Jugador::nuevoSoldado(int fila, int columna){
-	if(fila < 1 || columna < 1){
+void Jugador::nuevoSoldado(int fila, int columna) {
+	if (fila < 1 || columna < 1) {
 		throw "No se puede agregar un soldado en una fila o columna que no tenga valor al menos 1.";
 	}
-	
+
 	this->soldados->add(new Ficha(SOLDADO, fila, columna, 1));
 }
 
-void Jugador::nuevaHerramienta(char ficha, int fila, int columna, int altura){
-	if(fila < 1 || columna < 1 || altura < 1){
+void Jugador::nuevaHerramienta(char ficha, int fila, int columna, int altura) {
+	if (fila < 1 || columna < 1 || altura < 1) {
 		throw "No se puede agregar una herramienta en coordenadas inválidas.";
 	}
-	
-	if (this->cartas->contarElementos() < MAX_HERRAMIENTAS){
+
+	if (this->cartas->contarElementos() < MAX_HERRAMIENTAS) {
 		this->herramientas->add(new Ficha(ficha, fila, columna, altura));
 	} else {
 		throw "No se pueden poner mas herramientas";
 	}
 }
 
-void Jugador::eliminarSoldado(int fila, int columna){
-	if(fila < 1 || columna < 1){
+void Jugador::eliminarSoldado(int fila, int columna) {
+	if (fila < 1 || columna < 1) {
 		throw "No se puede eliminar un soldado en una posición inválida. La fila y la columna deben ser mayor o igual a 1.";
 	}
-	
+
 	this->soldados->remover(this->buscarIdSoldado(fila, columna));
 	this->cantidadSoldados--;
 }
 
-void Jugador::eliminarCarta(int id){
-	if(id < 0){
+void Jugador::eliminarCarta(int id) {
+	if (id < 0) {
 		throw "No se pudo eliminar la carta, el id ingresado es inválido.";
 	}
 	this->cartas->remover(id);
 }
 
-void Jugador::eliminarHerramienta(int id){
-	if(id < 0){
+void Jugador::eliminarHerramienta(int id) {
+	if (id < 0) {
 		throw "No se pudo eliminar la herramientas, el id ingresado es inválido.";
 	}
 	this->herramientas->remover(id);
 }
 
-Jugador::~Jugador(){
+Jugador::~Jugador() {
 	if (this->soldados) {
-		for (int i = 1; i < this->getCantidadDeSoldados(); i++){
+		for (int i = 1; i < this->getCantidadDeSoldados(); i++) {
 			this->soldados->remover(i);
 		}
 		delete[] this->soldados;
 	}
 	if (this->herramientas) {
-		for (unsigned int i = 1; i < this->getCantidadDeHerramientas(); i++){
+		for (unsigned int i = 1; i < this->getCantidadDeHerramientas(); i++) {
 			this->herramientas->remover(i);
 		}
 		delete[] this->herramientas;
 	}
 	if (this->cartas) {
-		for (unsigned int i = 0; i < this->getCantidadDeCartas(); i++){
+		for (unsigned int i = 0; i < this->getCantidadDeCartas(); i++) {
 			this->cartas->remover(i);
 		}
 		delete[] this->cartas;
